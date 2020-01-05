61a43392c480e5a58bb3a61e8f7c737203fbdc06
Roman Basharin <r.basharin@itigris.ru>
2020-01-05 21:14:36 +0300

    Упростил notifyUserAboutError

--- src/plugins/common/notifyUserAboutError.js
+++ src/plugins/common/notifyUserAboutError.js
@@ -1,40 +1,18 @@
 function $notifyUserAboutError(anyTypeError) {
 	if (typeof anyTypeError === 'string') {
-		this.$notify.error(anyTypeError)
-		return
-	}
+		this.$notify.error({
+			title: 'Ошибка',
+			message: anyTypeError,
+		})
 
-	if (!anyTypeError.response) {
-		this.$notify.error(anyTypeError.message)
 		return
 	}
 
 	const { response } = anyTypeError
 
-	if (!response.data) {
-		this.$notify.error(response)
-		return
-	}
-
-	if (response.data.errors) {
-		let message = ''
-
-		Object.keys(response.data.errors).forEach(e => {
-			message += `${e} ${response.data.errors[e].msg}<br>`
-		})
-
-		this.$notify.error({
-			title: response.status,
-			dangerouslyUseHTMLString: true,
-			message,
-		})
-
-		return
-	}
-
 	this.$notify.error({
-		title: response.data.status || response.status,
-		message: response.data.error || response.data.message,
+		title: response.statusText,
+		message: response.data,
 	})
 }
 
